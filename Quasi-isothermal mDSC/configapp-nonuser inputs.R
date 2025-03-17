a60591bc6bf8ae710a27268c4063cad9f911e0cb
Tom-kon <tom.konings@kuleuven.be>
2025-03-17 17:46:11 +0100

    some details

--- Quasi-isothermal mDSC/configapp-nonuser inputs.R
+++ Quasi-isothermal mDSC/configapp-nonuser inputs.R
@@ -5,7 +5,10 @@ points_distance_minimum_margin <- (sampling*period*60)/2/5
 plotTitleTHF <-paste0("NRHF based on FT (frequency = 0), ", modulations_back, " modulations")
 plottitleRHF <- paste0("RevCp based on FT (1st harmonic), ", modulations_back, " modulations")
 plottitleRHFmanual <- paste0("RevCp calculated manually, ", modulations_back, " modulations")
-subtitle <- paste0("Dataset: ", fileName)
+subtitle <- paste0("Dataset: ", "test")
 tempModAmplitude <- setAmplitude*2*pi/period
 num_ticks <- 10
+sample_size <- 5.79
+
 #-----------------------------------------------------
+
