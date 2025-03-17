543f6c9a34e64d583597c4832259d26d883c4f46
Tom-kon <tom.konings@kuleuven.be>
2025-03-17 17:48:16 +0100

    made sure these are in different columns + improved how everything looks

--- Quasi-isothermal mDSC/configapp.R
+++ Quasi-isothermal mDSC/configapp.R
@@ -1,22 +1,44 @@
-configUI <- function() {
+configUI1<- function() {
   tagList(
-    checkboxInput("plot_datasteps4", "Plot final raw data?", FALSE),
-    checkboxInput("saveplotdatasteps4", "Save final raw data?", FALSE),
-    checkboxInput("saveNRHFplot", "Save non-reversing heat flow plot?", FALSE),
-    checkboxInput("saveRHFplot", "Save reversing heat flow plot?", FALSE),
-    checkboxInput("savemanualRHFplot", "Save manually calculated reversing heat flow plot?", FALSE),
-    checkboxInput("saveDatasteps4", "Save final raw data?", FALSE),
-    checkboxInput("saveExtremadf3", "Plot final raw data?", FALSE),
-    checkboxInput("saveSummaryFT", "Plot final raw data?", FALSE),
-    
-    textInput("period", "What was your modulation period (in minutes)", "2/3"), 
-    textInput("step_size", "What was your step size (in °C)", "3"),
-    textInput("isotherm_length", "What was your isotherm length (in minutes)", "20"),
-    textInput("starting_temp", "What was your starting temperature (in °C)", "13"),
-    textInput("setAmplitude", "What was your temperature modulation amplitude (in °C)", "0.212")
-    
+    column(6,
+      checkboxInput("saveNRHFplot", "Save non-reversing heat flow plot?", FALSE),
+      checkboxInput("saveRHFplot", "Save reversing heat flow plot?", FALSE),
+      checkboxInput("savemanualRHFplot", "Save manually calculated reversing heat flow plot?", FALSE),
+      checkboxInput("saveDatasteps4", "Save final raw data?", FALSE),
+      checkboxInput("saveExtremadf3", "Save final maxima and minima list?", FALSE),
+      checkboxInput("saveSummaryFT", "Save final analysed data", FALSE),
+      fileInput("Excel_in", "Upload your Excel here"),    
+    )
   )
 }
+
+configUI2<- function() {
+  tagList(
+    column(6,
+      textInput("period_in", "What was your modulation period (in minutes)", "2/3"), 
+      textInput("step_size_in", "What was your step size (in °C)", "3"),
+      textInput("isotherm_length_in", "What was your isotherm length (in minutes)", "20"),
+      textInput("starting_temp_in", "What was your starting temperature (in °C)", "13"),
+      textInput("setAmplitude_in", "What was your temperature modulation amplitude (in °C)", "0.212"),
+      textInput("modulations_back_in", "How many modulations should be used for the final calculation?", "15"),
+    )
+  )
+}
+
+configUI3<- function() {
+  tagList(
+    HTML("<br>"),
+    HTML("<br>"),
+    tags$div(
+      style = "text-align: center;",
+      actionButton("calculate", "Calculate", class = "btn-primary btn-lg")
+    )
+  )
+}
+
+
+
+
 
 
 
