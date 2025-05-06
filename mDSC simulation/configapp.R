452b7b2c996772d50761bfd859c1c3ffe30061df
Tom6960 <tomkonings6960@gmail.com>
2025-05-06 23:54:12 +0200

    needed to add ns for the overarching app

--- mDSC simulation/configapp.R
+++ mDSC simulation/configapp.R
@@ -1,68 +1,65 @@
-configUI1<- function() {
+configUI1 <- function(ns) {
   tagList(
     column(6,
-           textInput("savetitle", "Title of the picture that will be saved", "test"),
-           checkboxInput("saveplots", "Do you want to save the plots?", value= FALSE),
-           textInput("sampling", "Sampling rate in points per second", "10"),
-           textInput("startTemp", "Starting temperature of the mDSC run (°C)", "20"),
-           textInput("endTemp", "Final temperature of the mDSC run (°C)", "180"),
-           textInput("period", "Period of the modulations in seconds", "40"),
-           textInput("heatRate", "Heating rate of the mDSC runs in °C/min", "2"),
-           textInput("Atemp", "Amplitude of the temperature modulation (°C)", "0.212"),
-           textInput("phase", "Phase of the modulated heat flow with respect to the temperature modulation (rad)", "-0.2"), 
-           textInput("loessAlpha", "Degree of smoothing (higher = more smoothing)", "0.05"),    
+           textInput(ns("savetitle"), "Title of the picture that will be saved", "test"),
+           checkboxInput(ns("saveplots"), "Do you want to save the plots?", value = FALSE),
+           textInput(ns("sampling"), "Sampling rate in points per second", "10"),
+           textInput(ns("startTemp"), "Starting temperature of the mDSC run (°C)", "20"),
+           textInput(ns("endTemp"), "Final temperature of the mDSC run (°C)", "180"),
+           textInput(ns("period"), "Period of the modulations in seconds", "40"),
+           textInput(ns("heatRate"), "Heating rate of the mDSC runs in °C/min", "2"),
+           textInput(ns("Atemp"), "Amplitude of the temperature modulation (°C)", "0.212"),
+           textInput(ns("phase"), "Phase of the modulated heat flow with respect to the temperature modulation (rad)", "-0.2"),
+           textInput(ns("loessAlpha"), "Degree of smoothing (higher = more smoothing)", "0.05")
     )
   )
 }
 
-configUI2<- function() {
+configUI2 <- function(ns) {
   tagList(
     column(4,
-           textInput("deltaRHFPreTg", "What is the slope of the reversing heat flow before the Tg (J/(g*°C))", "-0.000137"), 
-           textInput("deltaRHFPostTg", "What is the slope of the reversing heat flow after the Tg (J/(g*°C))", "-0.000120"),
-           textInput("StartRHFPreTg", "What value does your reversing heat flow start at (J/g)", "-0.040"),
-           textInput("deltaCpPreTg", "What is the slope of the total heat capacity before the Tg (J/(g*°C))", "0.00858"),
-           textInput("deltaCpPostTg", "What is the slope of the total heat capacity after the Tg (J/(g*°C))", "0.00204"),
-           textInput("StartCpTempPreTg", "What value does your total heat capacity start at (J/g)", "1.05"),
+           textInput(ns("deltaRHFPreTg"), "What is the slope of the reversing heat flow before the Tg (J/(g*°C))", "-0.000137"),
+           textInput(ns("deltaRHFPostTg"), "What is the slope of the reversing heat flow after the Tg (J/(g*°C))", "-0.000120"),
+           textInput(ns("StartRHFPreTg"), "What value does your reversing heat flow start at (J/g)", "-0.040"),
+           textInput(ns("deltaCpPreTg"), "What is the slope of the total heat capacity before the Tg (J/(g*°C))", "0.00858"),
+           textInput(ns("deltaCpPostTg"), "What is the slope of the total heat capacity after the Tg (J/(g*°C))", "0.00204"),
+           textInput(ns("StartCpTempPreTg"), "What value does your total heat capacity start at (J/g)", "1.05")
     )
   )
 }
 
-configUI3<- function() {
+configUI3 <- function(ns) {
   tagList(
     column(4,
-           textInput("locationTgTHF", "Where is the Tg on the total heat flow? Input start, end, and midpoint separated by commas (°C)", "28.46, 39.13, 33.77"), 
-           textInput("locationTgRHF", "Where is the Tg on the reversing heat flow? Input start, end, and midpoint separated by commas (°C)", "35, 45, 40"),
-           textInput("deltaCpTg", "What is the jump in heat capacity at the Tg (J/(g*°C))", "0.268"),
-           textInput("MeltEnth", "What is the melting enthalpy (in J/g)", "-0.2"),
-           textInput("phase_melt", "What is the phase of the melting with respect to the temperature modulation (rad)", "0"),
-           textInput("periodSignal", "Period of the melting modulations in seconds", "40"),
-           textInput("locationMelt", "Where does the melting start and stop? Two inputs separated by a comma, in °C", "134, 154"),
+           textInput(ns("locationTgTHF"), "Where is the Tg on the total heat flow? Input start, end, and midpoint separated by commas (°C)", "28.46, 39.13, 33.77"),
+           textInput(ns("locationTgRHF"), "Where is the Tg on the reversing heat flow? Input start, end, and midpoint separated by commas (°C)", "35, 45, 40"),
+           textInput(ns("deltaCpTg"), "What is the jump in heat capacity at the Tg (J/(g*°C))", "0.268"),
+           textInput(ns("MeltEnth"), "What is the melting enthalpy (in J/g)", "-0.2"),
+           textInput(ns("phase_melt"), "What is the phase of the melting with respect to the temperature modulation (rad)", "0"),
+           textInput(ns("periodSignal"), "Period of the melting modulations in seconds", "40"),
+           textInput(ns("locationMelt"), "Where does the melting start and stop? Two inputs separated by a comma, in °C", "134, 154")
     )
   )
 }
 
-configUI4<- function() {
+configUI4 <- function(ns) {
   tagList(
     column(4,
-           textInput("Crystalenth", "What is the crystallisation enthalpy (J/g)", "0.05"), 
-           textInput("locationcrystal", "Where does the crystallisation start and stop? Two inputs separated by a comma, in °C", "80,100"),
-           textInput("EnthrecEnth", "What is the enthalpy recovery enthalpy (J/g)", "-0.02"),
-           textInput("locationEnthRec", "Where does the enthalpy recovery start and stop? Two inputs separated by a comma, in °C", "30,45"),
+           textInput(ns("Crystalenth"), "What is the crystallisation enthalpy (J/g)", "0.05"),
+           textInput(ns("locationcrystal"), "Where does the crystallisation start and stop? Two inputs separated by a comma, in °C", "80,100"),
+           textInput(ns("EnthrecEnth"), "What is the enthalpy recovery enthalpy (J/g)", "-0.02"),
+           textInput(ns("locationEnthRec"), "Where does the enthalpy recovery start and stop? Two inputs separated by a comma, in °C", "30,45")
     )
   )
 }
 
-configUI5<- function() {
+configUI5 <- function(ns) {
   tagList(
     HTML("<br>"),
     HTML("<br>"),
     tags$div(
       style = "text-align: center;",
-      actionButton("calculate", "Calculate", class = "btn-primary btn-lg")
+      actionButton(ns("calculate"), "Calculate", class = "btn-primary btn-lg")
     )
   )
 }
-
-
-
