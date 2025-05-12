774ccb0985688ba0c80274f3c272a3132338cb4d
Tom-kon <tom.konings@kuleuven.be>
2025-05-12 15:57:30 +0200

    necessary changes (adding ns) and getting modularized app ready

--- Quasi-isothermal mDSC/configapp.R
+++ Quasi-isothermal mDSC/configapp.R
@@ -1,38 +1,38 @@
 
 options(shiny.maxRequestSize = 100 * 1024^2)  # 100 MB limit
 
-configUI1<- function() {
+configUI1<- function(ns) {
   tagList(
     column(6,
-      checkboxInput("saveNRHFplot", "Save non-reversing heat flow plot?", FALSE),
-      checkboxInput("saveRevCpplot", "Save reversing heat flow plot?", FALSE),
-      checkboxInput("savemanualRevCpplot", "Save manually calculated reversing heat flow plot?", FALSE),
-      checkboxInput("saveExcel", "Save Excel with all the analyses?", TRUE),
-      fileInput("Excel_in", "Upload your Excel here"),    
+      checkboxInput(ns("saveNRHFplot"), "Save non-reversing heat flow plot?", FALSE),
+      checkboxInput(ns("saveRevCpplot"), "Save reversing heat flow plot?", FALSE),
+      checkboxInput(ns("savemanualRevCpplot"), "Save manually calculated reversing heat flow plot?", FALSE),
+      checkboxInput(ns("saveExcel"), "Save Excel with all the analyses?", TRUE),
+      fileInput(ns("Excel_in"), "Upload your Excel here"),    
     )
   )
 }
 
-configUI2<- function() {
+configUI2<- function(ns) {
   tagList(
     column(6,
-      textInput("period_in", "What was your modulation period (in minutes)", "2/3"), 
-      textInput("step_size_in", "What was your step size (in °C)", "3"),
-      textInput("isotherm_length_in", "What was your isotherm length (in minutes)", "20"),
-      textInput("starting_temp_in", "What was your starting temperature (in °C)", "13"),
-      textInput("setAmplitude_in", "What was your temperature modulation amplitude (in °C)", "0.212"),
-      textInput("modulations_back_in", "How many modulations should be used for the final calculation?", "15"),
+      textInput(ns("period_in"), "What was your modulation period (in minutes)", "2/3"), 
+      textInput(ns("step_size_in"), "What was your step size (in °C)", "3"),
+      textInput(ns("isotherm_length_in"), "What was your isotherm length (in minutes)", "20"),
+      textInput(ns("starting_temp_in"), "What was your starting temperature (in °C)", "13"),
+      textInput(ns("setAmplitude_in"), "What was your temperature modulation amplitude (in °C)", "0.212"),
+      textInput(ns("modulations_back_in"), "How many modulations should be used for the final calculation?", "15"),
     )
   )
 }
 
-configUI3<- function() {
+configUI3<- function(ns) {
   tagList(
     HTML("<br>"),
     HTML("<br>"),
     tags$div(
       style = "text-align: center;",
-      actionButton("calculate", "Calculate", class = "btn-primary btn-lg")
+      actionButton(ns("calculate"), "Calculate", class = "btn-primary btn-lg")
     )
   )
 }
---
+++ Quasi-isothermal mDSC/modularized_quasiisothermalmDSC_main.R
@@ -0,0 +1,147 @@
+source("../Quasi-isothermal mDSC/libraries.R")
+source("../Quasi-isothermal mDSC/configapp.R")
+
+quasiIsotherm_ui <- function(id) {
+  ns <- NS(id)
+  ui <- navbarPage(
+    id = ns("tabs"),
+    title = "Quasi-isothermal mDSC data analyzer",
+    # theme = bs_theme(preset = "lumen"),
+    inverse = FALSE,  # if you want a dark navbar style; remove if not needed
+    tabPanel(
+      title = "Input",
+      id = ns("input"),
+      icon = icon("gears", class = "fa-solid"),
+      fluidPage(
+        configUI1(ns), configUI2(ns), configUI3(ns)
+      )
+    ),
+    tabPanel(
+      id = ns("graphs"),
+      title = "Graphs",
+      icon = icon("chart-line", class = "fa-solid"),
+      fluidPage(
+        titlePanel("Output graphs"),
+        fluidRow(
+          column(12, wellPanel(
+            selectInput(ns("plot_choice"), "Select Plot:", 
+                        choices = c("NRHF", "RevCp", "Manual RevCp", "RevCp and NRHF", 
+                                    "Maxima and minima 1", "Maxima and minima prefinal", "Maxima and minima final", 
+                                    "Original data", "First cleaned up data", "Prefinal cleaned up data", "Final data used for analysis"), 
+                        selected = "RevCp"),
+            fluidRow(
+              column(6,
+                     HTML("<br>"),
+                     actionButton(ns("recalc"), "Recalculate with different number of modulations")
+              ),
+              column(6, 
+                     textInput(ns("modulations_back_in_new"), "New number of modulations")
+              )
+            )
+          ))
+        ),
+        fluidRow(
+          column(12, plotlyOutput(ns("plot"), height = "90vh"))
+        )
+      )
+    )
+    
+    
+  )
+}
+
+
+mdsc_quasiIso_server <- function(id) {
+  moduleServer(id, function(input, output, session) {
+    # Create a reactiveValues object to store inputs
+    reactive_inputs <- reactiveValues()
+    
+    # This part is separate and independent of your server logic
+    observeEvent(input$calculate, {
+      # Handle checkboxes
+      reactive_inputs$saveNRHFplot <- as.logical(input$saveNRHFplot)
+      reactive_inputs$saveRevCpplot <- as.logical(input$saveRevCpplot)
+      reactive_inputs$savemanualRevCpplot <- as.logical(input$savemanualRevCpplot)
+      reactive_inputs$saveExcel <- as.logical(input$saveExcel)
+      
+      # Store numerical/text inputs with proper reactive evaluation
+      reactive_inputs$period <- eval(parse(text = input$period_in))  # Assuming input is numeric
+      reactive_inputs$stepSize <- eval(parse(text = input$step_size_in))
+      reactive_inputs$isothermLength <- eval(parse(text = input$isotherm_length_in))
+      reactive_inputs$startingTemp <- eval(parse(text = input$starting_temp_in))
+      reactive_inputs$modulations_back <- eval(parse(text = input$modulations_back_in))
+      reactive_inputs$setAmplitude <- eval(parse(text = input$setAmplitude_in))
+      
+    
+    # Update reactive values when calculate button is pressed
+    
+      
+      # Store file input
+      reactive_inputs$Excel <- input$Excel_in$datapath
+      reactive_inputs$fileName <- input$Excel_in$name
+      
+      # Source necessary scripts
+      source("../Quasi-isothermal mDSC/Processing and cleaning overall function.R")
+      source("../Quasi-isothermal mDSC/plots.R")
+      
+      # Call the processing function and store results in reactive value
+      sample_results <- processDSC(
+        fileName = reactive_inputs$fileName,
+        Excel = reactive_inputs$Excel,
+        export = TRUE,
+        starting_temp = reactive_inputs$startingTemp,
+        step_size = reactive_inputs$stepSize,
+        modulations_back = reactive_inputs$modulations_back,
+        period = reactive_inputs$period,
+        isothermLength = reactive_inputs$isothermLength,
+        setAmplitude = reactive_inputs$setAmplitude,
+        saveNRHFplot = reactive_inputs$saveNRHFplot,
+        saveRevCpplot = reactive_inputs$saveRevCpplot,
+        savemanualRevCpplot = reactive_inputs$savemanualRevCpplot,
+        saveExcel = reactive_inputs$saveExcel
+      )
+      
+      # Store the processed results in reactiveValues()
+      reactive_inputs$sample_results <- sample_results
+    })
+    
+    # Handle recalculation with a different number of modulations
+    observeEvent(input$recalc, {
+      source("../Quasi-isothermal mDSC/quickly recalculate for different mods.R")
+      
+      reactive_inputs$modulations_back <- eval(parse(text = input$modulations_back_in_new))
+      
+      reactive_inputs$sample_results <- processDSCrecalc(
+        fileName = reactive_inputs$fileName,
+        sample_results = reactive_inputs$sample_results,
+        modulations_back = reactive_inputs$modulations_back,
+        period = reactive_inputs$period,
+        setAmplitude = reactive_inputs$setAmplitude,
+        starting_temp = reactive_inputs$startingTemp,
+        step_size = reactive_inputs$stepSize,
+        saveExcel = reactive_inputs$saveExcel
+      )
+    })
+    
+    # Render the plot using the reactive sample_results
+    output$plot <- renderPlotly({
+      req(reactive_inputs$sample_results)  # Ensure results exist
+      res <- reactive_inputs$sample_results
+      
+      plot_obj <- switch(input$plot_choice,
+                         "NRHF" = NRHF_plot(res$ft_averages, reactive_inputs$modulations_back, reactive_inputs$fileName, reactive_inputs$saveNRHFplot),
+                         "RevCp" = RevCp_plot(res$ft_averages, reactive_inputs$modulations_back, reactive_inputs$fileName, reactive_inputs$saveRevCpplot),
+                         "Manual RevCp" = Manual_RevCp_plot(res$average_heat_flow_per_pattern, reactive_inputs$modulations_back, reactive_inputs$fileName, reactive_inputs$savemanualRevCpplot),
+                         "RevCp and NRHF" = RevCp_NRHF_plot(res$ft_averages, reactive_inputs$modulations_back, reactive_inputs$fileName),
+                         "Maxima and minima 1" = Maxima_minima(res$`Extrema df1`, reactive_inputs$modulations_back, reactive_inputs$fileName),
+                         "Maxima and minima prefinal" = Maxima_minima_1(res$`Extrema df2`, reactive_inputs$modulations_back, reactive_inputs$fileName),
+                         "Maxima and minima final" =  Maxima_minima_2(res$`Extrema df3`, reactive_inputs$modulations_back, reactive_inputs$fileName),
+                         "Original data" = Original_data(res$`Original data`, reactive_inputs$modulations_back, reactive_inputs$fileName),
+                         "First cleaned up data" = Datasteps_plot_1(res$d_steps_cleaned, reactive_inputs$modulations_back, reactive_inputs$fileName),
+                         "Prefinal cleaned up data" = Datasteps_plot_prefinal(res$d_steps_cleaned_2, reactive_inputs$modulations_back, reactive_inputs$fileName),
+                         "Final data used for analysis" = Datasteps_plot_final(res$d_steps_cleaned_3, reactive_inputs$modulations_back, reactive_inputs$fileName))
+      
+      ggplotly(plot_obj, tooltip = c("x", "y", "text"))
+    })
+  })
+}
