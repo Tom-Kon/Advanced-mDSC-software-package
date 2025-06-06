a8c3a740ce046ed102f5ae0ae50e30a6225ec4b3
Tom-kon <tom.konings@kuleuven.be>
2025-06-06 16:01:25 +0200

    add some UI options, features, error handling

--- Code/Regular modulated DSC deconvolution/Regular mDSC deconvolution.R
+++ Code/Regular modulated DSC deconvolution/Regular mDSC deconvolution.R
@@ -1,5 +1,6 @@
 source("../Regular modulated DSC deconvolution/functions.R")
 source("../Regular modulated DSC deconvolution/Libraries.R")
+source("../Regular modulated DSC deconvolution/error handling.R")
 
 
 normal_mDSC_ui <- function(id) {
@@ -28,7 +29,8 @@ normal_mDSC_ui <- function(id) {
                  actionButton(ns("analyze"), "Analyze", class = "btn-primary btn-lg")
           ), 
           column(6,
-                 checkboxInput(ns("compare"), "Do you want to compare with regular DSC?"),
+                 checkboxInput(ns("HFcalcextra"), "Do you want to calculate RHF and NRHF using the THF as well?"),
+                 checkboxInput(ns("compare"), "Do you want to compare with unmodulated DSC?"),
                  conditionalPanel(
                    condition = sprintf("input['%s']", ns("compare")),
                    fileInput(ns("Excel_DSC"), "Upload your Excel here"),
@@ -38,6 +40,12 @@ normal_mDSC_ui <- function(id) {
                      selectInput(ns("sheet2"), "What sheet is it in then?", choices = c("2", "3", "4", "5"))
                    ),                    
                  ),
+                 mainPanel(
+                   div(
+                     class = "error-text",
+                     textOutput(ns("errorMessage"))
+                   )    
+                 ) 
           )
         )
       )
@@ -50,13 +58,15 @@ normal_mDSC_ui <- function(id) {
         fluidRow(
           column(12, wellPanel(
             selectInput(ns("plot_choice"), "Select Plot:", 
-                        choices = c("THF", "RHF","NRHF", "THF FT", "RHF FT", "THF TRIOS", "RHF based on THF TRIOS", "NRHF based on THF TRIOS"), 
+                        choices = c("THF", "RHF","NRHF", "THF FT", "RHF FT", 
+                                    "THF TRIOS", "RHF based on THF TRIOS", "NRHF based on THF TRIOS", 
+                                    "THF unmodulated DSC", "RHF based on unmodulated DSC", "NRHF based on unmodulated DSC"), 
                         selected = "THF"),
           ))
         ),
         fluidRow(
           column(12, plotlyOutput(ns("plot"), height = "90vh"))
-        )
+        ), 
       )
     )
   )
@@ -76,10 +86,12 @@ normal_mDSC_server <- function(id) {
       reactive_inputs$period <- as.numeric(input$period)
       reactive_inputs$heating_rate <- as.numeric(input$heating_rate)
       reactive_inputs$setAmplitude <- as.numeric(input$setAmplitude)
-      reactive_inputs$heat_amplitude <- reactive_inputs$setAmplitude*2*pi/reactive_inputs$period
       reactive_inputs$compare <- input$compare
       reactive_inputs$ExcelmDSC <- input$Excel_mDSC$datapath
       reactive_inputs$fileName <- input$Excel_in$name
+      reactive_inputs$HFcalcextra <- input$HFcalcextra
+      reactive_inputs$compare <- input$compare
+      
       
       if(input$sheetask) {
         reactive_inputs$sheet <- 1
@@ -95,42 +107,106 @@ normal_mDSC_server <- function(id) {
       
       
       # Read data from Excel sheets
-      d <- excel_cleaner(reactive_inputs$ExcelmDSC, reactive_inputs$sheet)
+      import <- 1
+      msg <- NULL
+      error_handling(reactive_inputs, import)
+      
+      
+      output$errorMessage <- renderText({
+        if (!is.null(msg)) {
+          msg
+        } else if (!is.null(attr(d, "comment"))) {
+          (attr(d, "comment"))
+        } else {""
+        }
+      })
+      
+      if (!is.null(msg)) hidePageSpinner()
+      
+      reactive_inputs$heat_amplitude <- reactive_inputs$setAmplitude*2*pi/reactive_inputs$period
+      
+      d <- excel_cleaner(reactive_inputs$ExcelmDSC, reactive_inputs$sheet, reactive_inputs$HFcalcextra, reactive_inputs$compare,import)
+      
+      # Update the error message output (this triggers UI update)
+      if(typeof(d) == "character") {
+        msg <- d
+      }
+      
+      output$errorMessage <- renderText({
+        if (!is.null(msg)) {
+          msg
+        } else if (!is.null(attr(d, "comment"))) {
+          (attr(d, "comment"))
+        } else {""
+          }
+      })
+      
+      if (!is.null(msg)) hidePageSpinner()
+      
+      req(is.null(msg))  # Exit here if there's an error
       
       #Apply functions for non-FT calculation
       extrema_df <-locate_extrema_manual(d$modHeatFlow, d$time, d$temperature)
       counts <- count_extrema(extrema_df)
       RHFdf <- HFcalc(extrema_df, reactive_inputs$heat_amplitude, reactive_inputs$heating_rate)
-      HFcalc2 <- TRUE
-      if(HFcalc2) {
-        RHFdf2 <- HFcalc2(extrema_df, reactive_inputs$heat_amplitude, reactive_inputs$heating_rate, d)
-      }
       reactive_inputs$RHFdf <- RHFdf
-      reactive_inputs$RHFdf2 <- RHFdf2
+      
+      
+      if(reactive_inputs$HFcalcextra) {
+        RHFdf2 <- HFcalc2(extrema_df, reactive_inputs$heat_amplitude, reactive_inputs$heating_rate, d)
+        reactive_inputs$RHFdf2 <- RHFdf2
+      }
+
+
 
       #Apply functions for FT calculation
       reactive_inputs$fftCalc <- fftCalc(reactive_inputs$period, d, reactive_inputs$heat_amplitude, reactive_inputs$heating_rate)
       
       
       #Compare with DSC
-      if(reactive_inputs$compare == TRUE) {
-        
+      if(reactive_inputs$compare) {
         reactive_inputs$ExcelDSC <- input$Excel_DSC$datapath
-        DSC <- excel_cleaner(reactive_inputs$ExcelDSC, reactive_inputs$sheet2)
+        import <- 2
         
-        # Match maxima in the modulated THF to points in the deconvoluted THF
-        matchingDSCmDSC <- RHFdf %>%
-          rowwise() %>%
-          mutate(
-            closest_index = which.min(abs(DSC$temperature - meantemp)),
-            heat_flowDSC = DSC$heaFlow[closest_index],
-            temperatureDSC = DSC$temperature[closest_index]
-          ) %>%
-          ungroup()
+        error_handling(reactive_inputs, import)
         
-        DSCdf <- data.frame(temperatureDSC, heat_flowDSC)
+        output$errorMessage <- renderText({
+          if (!is.null(msg)) {
+            msg
+          } else if (!is.null(attr(d, "comment"))) {
+            (attr(d, "comment"))
+          } else {""
+          }
+        })
+        
+        if (!is.null(msg)) hidePageSpinner()
+        
+        DSC <- excel_cleaner(reactive_inputs$ExcelmDSC, reactive_inputs$sheet, reactive_inputs$HFcalcextra, reactive_inputs$compare,import)
+        
+        
+        # Update the error message output (this triggers UI update)
+        if(typeof(d) == "character") {
+          msg <- d
+        }
+        
+        output$errorMessage <- renderText({
+          if (!is.null(msg)) {
+            msg
+          } else if (!is.null(attr(d, "comment"))) {
+            (attr(d, "comment"))
+          } else {""
+          }
+        })
+        
+        if (!is.null(msg)) hidePageSpinner()
+        
+        req(is.null(msg))  # Exit here if there's an error
+        
+        
+        DSCdf <- funcMatchingDSCMDSC(DSC, extrema_df, reactive_inputs$heat_amplitude, reactive_inputs$heating_rate)
+        reactive_inputs$DSCdf <- DSCdf
+   
       }
-      
       
       hidePageSpinner()
 
@@ -147,9 +223,30 @@ normal_mDSC_server <- function(id) {
                          "NRHF" = NRHFplot(reactive_inputs$RHFdf),
                          "THF FT" = THFplotFT(reactive_inputs$fftCalc),
                          "RHF FT" = RHFplotFT(reactive_inputs$fftCalc),
-                         "THF TRIOS" = THFplotTRIOS(reactive_inputs$RHFdf2),
-                         "RHF based on THF TRIOS" = RHFplotTRIOS(reactive_inputs$RHFdf2), 
-                         "NRHF based on THF TRIOS" = NRHFplotTRIOS(reactive_inputs$RHFdf2) 
+                         "THF TRIOS" = {
+                           validate(need(input$HFcalcextra, "Please check 'Do you want to calculate RHF and NRHF using the THF as well?' to show THF TRIOS plots."))
+                           THFplotTRIOS(reactive_inputs$RHFdf2)
+                         },
+                         "RHF based on THF TRIOS" = {
+                           validate(need(input$HFcalcextra, "Please check 'Do you want to calculate RHF and NRHF using the THF as well?' to show RHF based on THF TRIOS plots."))
+                           RHFplotTRIOS(reactive_inputs$RHFdf2)
+                         },
+                         "NRHF based on THF TRIOS" = {
+                           validate(need(input$HFcalcextra, "Please check 'Do you want to calculate RHF and NRHF using the THF as well?' to show NRHF based on THF TRIOS plots."))
+                           NRHFplotTRIOS(reactive_inputs$RHFdf2)
+                         },
+                         "THF unmodulated DSC" = {
+                           validate(need(input$compare, "Please check 'Do you want to compare with unmodulated DSC?' to show THF unmodulated DSC plots."))
+                           THFplotDSC(reactive_inputs$DSCdf)
+                         },
+                         "RHF based on unmodulated DSC" = {
+                           validate(need(input$compare, "Please check 'Do you want to compare with unmodulated DSC?' to show RHF unmodulated DSC plots."))
+                           RHFplotDSC(reactive_inputs$DSCdf)
+                         },
+                         "NRHF based on unmodulated DSC" = {
+                           validate(need(input$compare, "Please check 'Do you want to compare with unmodulated DSC?' to show NRHF unmodulated DSC plots."))
+                           NRHFplotDSC(reactive_inputs$DSCdf)
+                         }
       )
       
       ggplotly(plot_obj)
