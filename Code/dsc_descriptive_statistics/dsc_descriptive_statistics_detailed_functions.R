08cc228f128a653134f640e10a0baabb662d7720
Tom Konings <u0155764@kuleuven.be>
2026-01-13 13:47:30 +0100

    fixing Cp error AGAIN

--- Code/dsc_descriptive_statistics/dsc_descriptive_statistics_detailed_functions.R
+++ Code/dsc_descriptive_statistics/dsc_descriptive_statistics_detailed_functions.R
@@ -19,10 +19,10 @@ ordinalSuffix <- function(x) {
 
 # Function for cleaning values
 cleanAndConvert <- function(x) {
+  x <- gsub("J/\\(g\\.°C\\)", "", x)
   x <- gsub("°C", "", x)
   x <- gsub("J/g", "", x)
   x <- gsub("%", "", x)
   x <- gsub("mg", "", x)
-  x <- gsub("J/(g.°C)", "", x)
   x <- gsub(",", ".", x)
 }
